9b1e014f9f53dc99c2d73439e3b012d7b7fe129d
nick <nick104@outlook.com>
2024-05-16 03:11:43 -0300

    first commit

---
+++ src/main/java/com/itsmenicky/bibliotecaAcademica/BibliotecaAcademicaApplication.java
@@ -0,0 +1,13 @@
+package com.itsmenicky.bibliotecaAcademica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaAcademicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaAcademicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsmenicky/bibliotecaAcademica/BibliotecaAcademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsmenicky.bibliotecaAcademica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaAcademicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
