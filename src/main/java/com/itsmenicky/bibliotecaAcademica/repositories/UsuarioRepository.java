4b8ceead3801d3f9c917bca28528325a129bdf16
nick <nick104@outlook.com>
2024-05-25 00:51:54 -0300

    remove: delete usuario repository

--- src/main/java/com/itsmenicky/bibliotecaAcademica/repositories/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.itsmenicky.bibliotecaAcademica.repositories;
-import com.itsmenicky.bibliotecaAcademica.models.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsuarioRepository extends CrudRepository<Usuario, String>{
-    Usuario findById(long id);
-}
