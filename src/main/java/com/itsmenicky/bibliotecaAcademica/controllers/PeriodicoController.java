5d08e267fa2171c04d3d56cbe44accf622568822
nick <nick104@outlook.com>
2024-05-25 17:03:34 -0300

    fix: form-periodico file name

--- src/main/java/com/itsmenicky/bibliotecaAcademica/controllers/PeriodicoController.java
+++ src/main/java/com/itsmenicky/bibliotecaAcademica/controllers/PeriodicoController.java
@@ -9,8 +9,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
 
 @Controller
 public class PeriodicoController {
@@ -18,16 +22,27 @@ public class PeriodicoController {
     private PeriodicoRepository pr;
 
     //Chamando form de cadastro de periódicos
-    @RequestMapping("periodico/cadastrar-periodico")
+    @RequestMapping("/cadastrar-periodico")
     public String cadastrarPeriodico(){
-        return "periodico/formPeriodico";
+        return "periodico/form-periodico";
     }
 
     //Cadastrando periodicos
-    @RequestMapping(value = "periodico/cadastrar-periodico", method = RequestMethod.POST)
-    public String cadastrarPeriodico(@Valid Periodico periodico, BindingResult result, RedirectAttributes attributes){
+    @RequestMapping(value = "/cadastrar-periodico", method = RequestMethod.POST)
+    public String cadastrarPeriodico(@Valid Periodico periodico, BindingResult result, RedirectAttributes attributes, @RequestParam("imagem") MultipartFile imagem){
         if(result.hasErrors()){
             attributes.addFlashAttribute("mensagem", "Verifique os campos!");
+            System.out.println("erro" + result.getAllErrors());
+            return "redirect:/cadastrar-periodico";
+        }
+
+        try {
+            if(!imagem.isEmpty()){
+                periodico.setCapa_periodico(imagem.getBytes());
+            }
+        }catch (IOException e){
+            attributes.addFlashAttribute("mensagem", "Erro ao enviar imagem");
+            System.out.println("erro imagem" + result.getAllErrors());
             return "redirect:/cadastrar-periodico";
         }
 
@@ -39,7 +54,7 @@ public class PeriodicoController {
     //Listar periodicos
     @RequestMapping("/periodicos")
     public ModelAndView listarLivros(){
-        ModelAndView mv = new ModelAndView("/periodico/lista-periodicos");
+        ModelAndView mv = new ModelAndView("/periodico/listar-periodicos");
         Iterable<Periodico> periodicos = pr.findAll();
         mv.addObject("periodicos", periodicos);
         return mv;
