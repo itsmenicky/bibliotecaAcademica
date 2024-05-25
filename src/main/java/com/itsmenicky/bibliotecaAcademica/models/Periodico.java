fc61f91b184c3de818bd1c1f84790d1fba1c14d7
nick <nick104@outlook.com>
2024-05-25 17:02:58 -0300

    feat: create attributes data_publicacao and capa_periodico

--- src/main/java/com/itsmenicky/bibliotecaAcademica/models/Periodico.java
+++ src/main/java/com/itsmenicky/bibliotecaAcademica/models/Periodico.java
@@ -3,14 +3,28 @@ import jakarta.persistence.*;
 import lombok.Data;
 import jakarta.validation.constraints.*;
 import java.io.Serializable;
+import java.util.Base64;
+import java.util.Date;
 
 @Entity
 @Data
 public class Periodico implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @NotEmpty
     private String titulo;
+
+    @NotEmpty
+    private String data_publicacao;
+
+    @Lob
+    private byte[] capa_periodico;
+
+    public String getImagemBase64(){
+        return Base64.getEncoder().encodeToString(this.capa_periodico);
+    }
 }
